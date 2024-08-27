4977efd9e018778718eb59088e2ad2d1a6b37c38
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-08-27 18:28:19 +0530

    added into main file with provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 function App() {
 
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
