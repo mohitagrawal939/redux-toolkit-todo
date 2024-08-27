6e64a1948c342e1319d7fb465644e7d4e25aaa8a
Mohit Agrawal <mohitagrawal939@gmail.com>
2024-08-27 17:46:19 +0530

    add reducer functions

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -7,4 +7,24 @@ const initialState = {
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
-})
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            };
+            state.todos.push(todo);
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        },
+        updateTodo: (state, action) => {
+            const itemIndex = state.todos.findIndex(todo => todo.id === action.payload.id);
+            console.log(itemIndex);
+        }
+    }
+});
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
